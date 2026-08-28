bf1f74926de266e6f5209d3c5e5c85992849bb04
Ankit Modi <34683429+ankitmodi31198@users.noreply.github.com>
2026-08-28 19:20:23 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+
+        let duplicateFound = false;
+
+        if (nums.length && nums.length > 1) {
+            for(let ittr = 0 ; ittr < nums.length ; ittr++) {
+                for (let ittr2 = ittr + 1 ; ittr2 < nums.length ; ittr2++) {
+                    if (nums[ittr] == nums[ittr2]) {
+                        duplicateFound = true;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return duplicateFound;
+
+    }
+}
